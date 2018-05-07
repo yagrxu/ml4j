5a8c310eea3a1f8e8021c11095f8502ba1551344
Wei Xu <wei.xu@sap.com>
2018-05-07 21:48:46 +0200

    fix bug

--- datefinder/jd_util/datefinder/finder.py
+++ datefinder/jd_util/datefinder/finder.py
@@ -1,13 +1,14 @@
 import re
 from itertools import chain
 from jd_util.datefinder.const import JD_DATES
+from numpy import nan
 
 class DateFinder(object):
     def __init__(self, config=None):
         pass
     def finditer(self, text):
         # split space
-        if text is None or isinstance(text, str):
+        if text is None or text is nan or not isinstance(text, str):
             return []
         iters = []
         for regex in JD_DATES:
